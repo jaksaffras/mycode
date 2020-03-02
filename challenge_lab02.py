152d83c6ca2fc0ca002e16317a19590b7bd93bd0
Jaks Wildman <Jaks.Wildman@Gmail.com>
2020-03-02 16:17:06 -0500

    About complex list

--- challenge_lab02.py
+++ challenge_lab02.py
@@ -5,5 +5,7 @@ col_list.append(1492)
 
 input_name = input('Please type your name: ')
 
-print(f'In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {input_name} fell off the boat.')
+col_list.append(input_name)
+
+print(f'In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {col_list[3]} fell off the boat.')
 
