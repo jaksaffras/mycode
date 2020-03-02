dbc701d4b89f4f486ee673b01a38c4b897adcf13
Jaks Wildman <Jaks.Wildman@Gmail.com>
2020-03-02 13:31:27 -0500

    Add Labs 1-7

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+user_input = input("Please input your IPv4 Address: ")
+print("IPv4 Address provided: " + user_input)
+
+print("IPv4 Address provided: ", user_input)
+
+vendor_input = input("Vendor: ")
+
+print("Vendor provided:",vendor_input)
+
