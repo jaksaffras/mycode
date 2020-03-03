4a63c1f96deefdf339da6e3abbc4bcfd851102c7
Jaks Wildman <Jaks.Wildman@Gmail.com>
2020-03-03 13:40:39 -0500

    IfTest

---
+++ iftest/condition01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+hostname = input('Please provide the hostname: ')
+if hostname.lower()=='mtg':
+    print('The hostname was found to be MTG')
+    print('Hostname matches expected value')
+else:
+    print('Hostname was not found to be MTG')
+
+print('Exiting the script...')
+
